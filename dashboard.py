62186975a1855f40da8546ce41c2b899ebb48d9c
Parth Bhagat <parthb@nyu.edu>
2021-06-13 00:03:23 -0500

    add wsb scraping

--- dashboard.py
+++ dashboard.py
@@ -5,19 +5,23 @@ from scraping import *
 from alpaca import *
 
 option = st.sidebar.selectbox("Select Option",('home','wsb','optiondata'))
+symbol = st.sidebar.text_input("Ticker",value="AAPL",max_chars=5)
 
 if option == 'home':
 	st.header("select something in the nav bar")
 	st.subheader("stocks with high short interest, low runup") 
 	st.write("Runup is the maximum % difference between the 50MA and the price at any point in the last year, in my opinion a pretty good proxy for whether or not the stock has run-up already. Empirically tested a good cutoff to be 40%. Anything above 40% is pretty much a certainty the stock has already run up. The lower the runup % the more likely a runup has yet to occur, assuming all other signals check out.")
+	st.write("The relative frequency column is the percentage of ticker mentions the stock has received as a proportion of all ticker mentions on the WSB subreddit. Ultimately sentiment is not considered as the specific screen we are looking at is highly shorted stocks with low market cap and poor performance. It is not unreasonable to assume bullish sentiment from a subreddit known to pump such stocks.")
 	short_df = get_most_shorted_stocks()
 	runup_df = get_runup_data_for_stocks(short_df["Symbol"].tolist())
 	short_df = short_df.merge(runup_df,on="Symbol")
+	wsb_df = get_wsb_analysis()
+	print(wsb_df)
+	short_df = short_df.merge(wsb_df,how='left',on='Symbol')
 	st.dataframe(short_df[short_df["Runup (%)"] < 40])
 	st.markdown(get_table_download_link(short_df),unsafe_allow_html=True)
 elif option == 'optiondata':
 	st.header("Wait for the option data to render, it takes a while...")
-	symbol = st.sidebar.text_input("Ticker",value="AAPL",max_chars=5)
 	st.subheader("Underlying security information")
 	stock_quote = get_quote_for_security(symbol)
 	stock_quote
--- scraping.py
+++ scraping.py
@@ -1,6 +1,51 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+import os
+import praw
+import re
+
+def get_wsb_analysis(): # pulled from tomsant/wsbTrendingStonks
+
+    reddit = praw.Reddit(
+      client_id = os.getenv("REDDIT_CLIENT_ID"),
+      client_secret = os.getenv("REDDIT_SECRET"),
+      user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
+    )
+    df = []
+    for post in reddit.subreddit('wallstreetbets').hot(limit=500):
+        content = {
+        "title" : post.title,
+        "text" : post.selftext
+      }
+        df.append(content)
+    df = pd.DataFrame(df)
+    regex = re.compile('[^a-zA-Z ]')
+    word_dict = {}
+    for (index, row) in df.iterrows():
+        # titles
+        title = row['title']
+        title = regex.sub('', title)
+        title_words = title.split(' ')
+        # content
+        content = row['text']
+        content = regex.sub('', content)
+        content_words = content.split(' ')
+        # combine
+        words = title_words + content_words
+        for x in words:
+            if x in ['A', 'B', 'GO', 'ARE', 'ON']:
+                pass
+
+            elif x in word_dict:
+                word_dict[x] += 1
+            else:
+                word_dict[x] = 1
+    word_df = pd.DataFrame.from_dict(list(word_dict.items())).rename(columns = {0:"Symbol", 1:"Frequency"})
+    ticker_df = pd.read_csv('tickers.csv').rename(columns = { 'Name':'Company_Name'})
+    stonks_df = pd.merge(ticker_df, word_df, on='Symbol')
+    stonks_df["Relative Frequency (%)"] = stonks_df["Frequency"]*100/stonks_df["Frequency"].sum()
+    return stonks_df[["Symbol","Relative Frequency (%)"]]
 
 def get_first_val_in_table(table):
     first_row = table.find('tr')
